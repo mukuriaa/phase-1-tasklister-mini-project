722df655b968c99e9eeb750c886c0ace570d86ea
Eric Mukuria <mukuriaeric@gmail.com>
2022-06-14 23:49:47 -0700

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+
+  const taskForm = document.getElementById('create-task-form');
+  const dataInput = document.getElementById('new-task-description')
+  const tasksUl = document.getElementById('tasks')
+
+    taskForm.addEventListener('submit', (e) => {
+      e.preventDefault()
+      workApp()
+    })
+
+    const newTask = () => {
+      tasksUl.innerHTML = `<li>${dataInput.value}</li>`
+    }
+
+    const workApp = () => {
+      if(tasksUl.innerHTML) {
+        newTask()
+      } else {
+          console.log('fail')
+      }
+      
+    }    
 });
